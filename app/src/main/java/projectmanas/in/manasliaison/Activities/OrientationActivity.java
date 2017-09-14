c739750cea9153c31a922ed056ca9fd4bca6d4de
Kaushik Nath <kn.nath.kaushik@gmail.com>
2017-09-14 21:01:46 +0530

    All designs first try without the  interview  and result screen

---
+++ app/src/main/java/projectmanas/in/manasliaison/Activities/OrientationActivity.java
@@ -0,0 +1,15 @@
+package projectmanas.in.manasliaison.Activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import projectmanas.in.manasliaison.R;
+
+public class OrientationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_orientation);
+    }
+}
