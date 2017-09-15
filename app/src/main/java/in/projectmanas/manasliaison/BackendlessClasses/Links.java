6b85a36ce582b79ea07c4df2e9db6ec61e7636b6
KaushikNathMIT <kauanath@gmail.com>
2017-09-16 02:17:49 +0530

    Links class for online challenge, poster and registrationForm

---
+++ app/src/main/java/in/projectmanas/manasliaison/BackendlessClasses/Links.java
@@ -0,0 +1,35 @@
+package in.projectmanas.manasliaison.BackendlessClasses;
+
+/**
+ * Created by knnat on 9/16/2017.
+ */
+
+public class Links {
+    private String registrationForm;
+    private String onlineChallenge;
+    private String posterURI;
+
+    public String getRegistrationForm() {
+        return registrationForm;
+    }
+
+    public void setRegistrationForm(String registrationForm) {
+        this.registrationForm = registrationForm;
+    }
+
+    public String getOnlineChallenge() {
+        return onlineChallenge;
+    }
+
+    public void setOnlineChallenge(String onlineChallenge) {
+        this.onlineChallenge = onlineChallenge;
+    }
+
+    public String getPosterURI() {
+        return posterURI;
+    }
+
+    public void setPosterURI(String posterURI) {
+        this.posterURI = posterURI;
+    }
+}
